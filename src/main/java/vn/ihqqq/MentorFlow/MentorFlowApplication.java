9eb2ee626a5ae058f48d543505a541831efe99ed
ihqqq <trnphi127@gmail.com>
2025-10-05 16:41:26 +0700

    initialize project and set up codebase

---
+++ src/main/java/vn/ihqqq/MentorFlow/MentorFlowApplication.java
@@ -0,0 +1,13 @@
+package vn.ihqqq.MentorFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/ihqqq/MentorFlow/MentorFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ihqqq.MentorFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
